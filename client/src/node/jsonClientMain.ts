bfb148be118e76cf25c7d033899e628d1bc99374
Kalman Vince <30904009+DRKV333@users.noreply.github.com>
2026-07-27 18:45:02 +0000

    Change server debug port to not be random

--- client/src/node/jsonClientMain.ts
+++ client/src/node/jsonClientMain.ts
@@ -27,7 +27,7 @@ export async function activate(context: ExtensionContext) {
 	const serverModule = context.asAbsolutePath(serverMain);
 
 	// The debug options for the server
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=' + (6000 + Math.round(Math.random() * 999))] };
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6004'] };
 
 	// If the extension is launch in debug mode the debug server options are use
 	// Otherwise the run options are used
